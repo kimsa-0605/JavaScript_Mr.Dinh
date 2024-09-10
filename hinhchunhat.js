abfbc17d4387a5402aa069070d5736422e8fec16
kimsa-0605 <holykimsa05@gmail.com>
2024-09-10 08:33:19 +0700

    thaydoi

---
+++ hinhchunhat.js
@@ -0,0 +1,6 @@
+var cd=prompt("Mời bạn nhập chiều dài");
+var cr=prompt("Mời bạn nhập chiều rộng");
+var dt= cd*cr;
+// alert(dt);
+document.write(dt);
+// document.getElementById("kq").innerHTML= dt;
---
+++ script.js
@@ -0,0 +1,6 @@
+var a = "Hello";
+var b = "World!";
+var c = a+" "+b;
+// alert(c); // hiển thị dạng thông báo ngay tại trang đó
+// document.write(c); // in như bình thường ở một trang mới
+document.getElementById("kq").innerHTML= c; // tìm nơi có id là kq trỏ chuột và sẽ hiển thị kết quả ngay tại đó và thay thế chữ Kết quả là thành Hello World!
